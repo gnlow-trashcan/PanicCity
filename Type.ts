25f541a8c5a1650e96c2feaf2193c55f042c1b77
Gnlow <entrydark@gmail.com>
2020-11-09 16:04:38 +0900

    Update Type.ts

--- Type.ts
+++ Type.ts
@@ -1,14 +1,36 @@
+type ValueOf<T> = T[keyof T]
+
 const operatorFuncs = {
-    "==": (a: any, b: any) => a == b
+    "==": (a: any, b: any) => a == b,
+    "test": () => 0,
+    "test2": () => true
 }
+
+type OperatorOutput<N extends OperatorNames> = ReturnType<typeof operatorFuncs[N]>
+
+type OperatorInfos = { 
+    [K in keyof typeof operatorFuncs]: {
+        output: OperatorOutput<K>,
+        name: K,
+    } 
+}
+
+type OperatorByOutputType<O> = Extract<
+    ValueOf<OperatorInfos>, 
+    { output: O }
+> extends {name: infer N} ? N: never
+
 type OperatorNames = keyof typeof operatorFuncs
 
 type Operator<
     N extends OperatorNames = OperatorNames
 > = {[K in N]: Parameters<typeof operatorFuncs[K]>}
 
-type OperatorOutput<N extends OperatorNames> = ReturnType<typeof operatorFuncs[N]>
-
 interface Ability {
     if: Operator
 }
+
+type Onl<U> = U extends {name: infer N} ? N: never
+
+const a: ValueOf<OperatorInfos> & { output: boolean }
+const b: OperatorByOutputType<boolean>
