64a32d86db5fe071c25359c242903bbcbde86c2b
Gnlow <entrydark@gmail.com>
2020-11-10 08:44:27 +0900

    Update Type.ts

--- Type.ts
+++ Type.ts
@@ -2,8 +2,8 @@ type ValueOf<T> = T[keyof T]
 
 const operatorFuncs = {
     "==": (a: any, b: any) => a == b,
-    "test": () => 0,
-    "test2": () => true
+    ">=": (a: number, b: number) => a>= b,
+    "<=": (a: number, b: number) => a<= b,
 }
 
 type OperatorNames = keyof typeof operatorFuncs
@@ -40,3 +40,15 @@ type Operator<O> = {
         ? { [Q in keyof O]: O[Q] }
         : never
 }[OperatorNamesByOutputType<O>]
+
+interface Ability {
+    if: Operator<boolean>[]
+}
+
+const justice: Ability = {
+    if: [
+        { "==": [ { votes: { team: "mafia" } }, 0 ] },
+        { "==": [ { votes: { role: "cultReader" } }, 0 ] },
+        { ">=": [ { votes: { team: "citizenTeam" } }, { votes: { team: "cultTeam" } } ] }
+    ]
+}
