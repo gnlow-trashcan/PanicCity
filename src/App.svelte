db9bee71d7ea8c7fc6a8f496a2dde5033004f2d9
Gnlow <entrydark@gmail.com>
2020-11-13 10:38:54 +0900

    Add typescript & pusher

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,20 @@
-<script>
+<script lang="typescript">
+    import Pusher from "pusher-js"
 
+    Pusher.logToConsole = true
+
+    const pusher = new Pusher('568a922f75e7dbd9eb1a', {
+      cluster: 'ap3'
+    })
+
+    const channel = pusher.subscribe("my-channel")
+    channel.bind("my-event", data => {
+        alert(JSON.stringify(data))
+    })
 </script>
 
 <div class="App">
-
+    hi
 </div>
 
 <style>
